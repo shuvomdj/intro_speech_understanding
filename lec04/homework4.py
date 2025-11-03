0110272b7a2a25ca7fd9fc8bdb884acfb1a1dada
shuvomdj <shuvomdj@gmail.com>
2025-11-03 09:17:55 +0900

    Homework 4
    I have done homework 4

--- lec04/homework4.py
+++ lec04/homework4.py
@@ -12,7 +12,21 @@ def next_birthday(date, birthdays):
     birthday - the next day, after given date, on which somebody has a birthday
     list_of_names - list of all people with birthdays on that date
     '''
+    '''
     birthday = (1,1)
     list_of_names = []
     return birthday, list_of_names
-    
+    '''
+    month, day = date
+    while (month, day) not in birthdays:   # Counting month and day until we find it in birthdays
+        day = day + 1
+        if day > 31:                       # Easily 31 days month count
+            day = 1
+            month = month + 1
+            if month > 12:                 # Easily 12 months next year count
+                month = 1
+
+    birthday = (month, day)                # Here (month, day) confirms birthday
+    list_of_names = birthdays[birthday]
+    return birthday, list_of_names
+            
