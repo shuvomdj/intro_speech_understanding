64d8b506dae14e49ea2e98db96a3439f7a3e54c4
shuvomdj <shuvomdj@gmail.com>
2025-11-24 12:18:19 +0900

    HomeWork 8

--- lec08/homework8.py
+++ lec08/homework8.py
@@ -13,8 +13,8 @@ def waveform_to_frames(waveform, frame_length, step):
     frames (np.ndarray((num_frames, frame_length))) - waveform chopped into frames
        frames[m/step,n] = waveform[m+n] only for m = integer multiple of step
     '''
-    raise RuntimeError("You need to change this part")
-
+    return np.array( [waveform[m:m+frame_length] for m in range(0, len(waveform)-frame_length, step) ] )
+    
 def frames_to_mstft(frames):
     '''
     Take the magnitude FFT of every row of the frames matrix.
@@ -25,8 +25,8 @@ def frames_to_mstft(frames):
     @returns:
     mstft (np.ndarray((num_frames, frame_length))) - the magnitude short-time Fourier transform
     '''
-    raise RuntimeError("You need to change this part")
-
+    return np.abs(np.fft.fft(frames, axis=1))
+    
 def mstft_to_spectrogram(mstft):
     '''
     Convert max(0.001*amax(mstft), mstft) to decibels.
@@ -40,6 +40,6 @@ def mstft_to_spectrogram(mstft):
     The spectrogram should be expressed in decibels (20*log10(mstft)).
     np.amin(spectrogram) should be no smaller than np.amax(spectrogram)-60
     '''
-    raise RuntimeError("You need to change this part")
+    return 20 * np.log10 ( np.maximum( 0.001 * np.amax(mstft), mstft) )
 
 
