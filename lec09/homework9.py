9bb02f0028994c5faa470e6fbbb1303d5eecac96
Mark Hasegawa-Johnson <jhasegaw@illinois.edu>
2025-11-29 12:50:27 -0600

    lec09

---
+++ lec09/homework9.py
@@ -0,0 +1,52 @@
+import numpy as np
+
+def VAD(waveform, Fs):
+    '''
+    Extract the segments that have energy greater than 10% of maximum.
+    Calculate the energy in frames that have 25ms frame length and 10ms frame step.
+    
+    @params:
+    waveform (np.ndarray(N)) - the waveform
+    Fs (scalar) - sampling rate
+    
+    @returns:
+    segments (list of arrays) - list of the waveform segments where energy is 
+       greater than 10% of maximum energy
+    '''
+    raise RuntimeError("You need to change this part")
+
+def segments_to_models(segments, Fs):
+    '''
+    Create a model spectrum from each segment:
+    Pre-emphasize each segment, then calculate its spectrogram with 4ms frame length and 2ms step,
+    then keep only the low-frequency half of each spectrum, then average the low-frequency spectra
+    to make the model.
+    
+    @params:
+    segments (list of arrays) - waveform segments that contain speech
+    Fs (scalar) - sampling rate
+    
+    @returns:
+    models (list of arrays) - average log spectra of pre-emphasized waveform segments
+    '''
+    raise RuntimeError("You need to change this part")
+
+def recognize_speech(testspeech, Fs, models, labels):
+    '''
+    Chop the testspeech into segments using VAD, convert it to models using segments_to_models,
+    then compare each test segment to each model using cosine similarity,
+    and output the label of the most similar model to each test segment.
+    
+    @params:
+    testspeech (array) - test waveform
+    Fs (scalar) - sampling rate
+    models (list of Y arrays) - list of model spectra
+    labels (list of Y strings) - one label for each model
+    
+    @returns:
+    sims (Y-by-K array) - cosine similarity of each model to each test segment
+    test_outputs (list of strings) - recognized label of each test segment
+    '''
+    raise RuntimeError("You need to change this part")
+
+
