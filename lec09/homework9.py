fdd70491923646c3afc90706a53bf054878136f7
shuvomdj <shuvomdj@gmail.com>
2025-12-01 12:17:48 +0900

    HomeWork 9

--- lec09/homework9.py
+++ lec09/homework9.py
@@ -13,7 +13,16 @@ def VAD(waveform, Fs):
     segments (list of arrays) - list of the waveform segments where energy is 
        greater than 10% of maximum energy
     '''
-    raise RuntimeError("You need to change this part")
+    #raise RuntimeError("You need to change this part")
+    framelength = int(0.025 * Fs) 
+    frameskip = int(0.01 * Fs)
+    frames = np.array([waveform[m:m+framelength] for m in range(0, len(waveform)-framelength, frameskip)])
+    energy = np.sum(np.square(frames), axis=1)
+    VAD = [1 if energy[m] > 0.1*max(energy) else 0 for m in range(len(energy))]
+    start_times = [m*frameskip for m in range(len(VAD)) if VAD[m-1]==0 and VAD[m]==1 ]
+    end_times = [m*frameskip for m in range(len(VAD)) if VAD[m-1]==1 and VAD[m]==0]
+    segments = [waveform[start_times[k]:end_times[k]] for k in range(len(start_times))]
+    return segments
 
 def segments_to_models(segments, Fs):
     '''
@@ -29,7 +38,16 @@ def segments_to_models(segments, Fs):
     @returns:
     models (list of arrays) - average log spectra of pre-emphasized waveform segments
     '''
-    raise RuntimeError("You need to change this part")
+    #raise RuntimeError("You need to change this part")
+    N = int(0.004*Fs)  
+    S = int(0.002*Fs)  
+    models = []
+    for k in range(len(segments)):
+        frames = np.array([segments[k][m+1:m+N+1] - segments[k][m:m+N] for m in range(0, len(segments[k])-N, S)])
+        mstft = np.abs(np.fft.fft(frames, axis=1))
+        sgram = 20*np.log10(np.maximum(0.001*np.amax(mstft), mstft))
+        models.append( np.average(sgram[:, 0:int(N/2)], axis=0)) 
+    return models
 
 def recognize_speech(testspeech, Fs, models, labels):
     '''
@@ -47,6 +65,17 @@ def recognize_speech(testspeech, Fs, models, labels):
     sims (Y-by-K array) - cosine similarity of each model to each test segment
     test_outputs (list of strings) - recognized label of each test segment
     '''
-    raise RuntimeError("You need to change this part")
-
-
+    segments = VAD(testspeech, Fs)
+    testspectra = segments_to_models(segments, Fs)
+    Y = len(models)
+    K = len(testspectra)
+    sims = np.zeros((Y, K))
+    for y in range(Y):
+        for k in range(K):
+            x = testspectra[k]
+            m = models[y]
+            sims[y, k] = np.dot(x,m) / (np.sqrt(np.sum(np.square(m))) * np.sqrt(np.sum(np.square(x))))
+    test_outputs = []
+    for k in range(len(testspectra)):
+        test_outputs.append(labels[np.argmax(sims[:,k])])
+    return sims, test_outputs
